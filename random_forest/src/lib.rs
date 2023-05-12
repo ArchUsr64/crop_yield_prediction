bf7126e035066826ee365fc77fa993ee6124a4b6
ArchUsr64 <anshulusr@gmail.com>
2023-05-12 10:13:24 +0530

    implemented tree creating given instances from the dataset

--- random_forest/src/lib.rs
+++ random_forest/src/lib.rs
@@ -1,12 +1,100 @@
+#![allow(unused)]
 pub struct Instance {
-	features: [i32; 4],
-	value: i32,
+	feature: Features,
+	result: i32,
 }
-impl Instance {
-	pub fn new(features: [i32; 4], value: i32) -> Self {
-		Instance { features, value }
+
+#[derive(Clone)]
+struct Instances {
+	x: Vec<i32>,
+	y: Vec<i32>,
+	z: Vec<i32>,
+	w: Vec<i32>,
+	result: Vec<i32>,
+}
+impl Instances {
+	fn variance(&self) -> [f32; 4] {
+		fn sample_variance(data: &[i32]) -> f32 {
+			let sum_of_squares: i32 = data.iter().map(|i| i.pow(2)).sum();
+			let square_of_sum: i32 = data.iter().sum::<i32>().pow(2);
+			let n: f32 = data.len() as f32;
+			(sum_of_squares as f32 - (square_of_sum as f32 / n)) / (n - 1.)
+		}
+		[
+			sample_variance(&self.x),
+			sample_variance(&self.y),
+			sample_variance(&self.z),
+			sample_variance(&self.w),
+		]
+	}
+	fn sort_by_feature(&mut self, feature_index: usize) {
+		assert!(
+			(0..4).contains(&feature_index),
+			"Request feature index out of bounds"
+		);
+		let feature_vec = match feature_index {
+			0 => self.x.clone(),
+			1 => self.y.clone(),
+			2 => self.z.clone(),
+			_ => self.w.clone(),
+		};
+		let mut feature_vec_indexed = feature_vec.iter().enumerate().collect::<Vec<_>>();
+		feature_vec_indexed.sort_by(|a, b| a.1.cmp(b.1));
+		let new_index = feature_vec_indexed.iter().map(|(i, _)| i);
+		let len = self.result.len();
+		let mut sorted_instance = Self {
+			x: Vec::with_capacity(len),
+			y: Vec::with_capacity(len),
+			z: Vec::with_capacity(len),
+			w: Vec::with_capacity(len),
+			result: Vec::with_capacity(len),
+		};
+		new_index.for_each(|new| {
+			sorted_instance.x.push(self.x[*new]);
+			sorted_instance.y.push(self.y[*new]);
+			sorted_instance.z.push(self.z[*new]);
+			sorted_instance.w.push(self.w[*new]);
+			sorted_instance.result.push(self.result[*new])
+		});
+		*self = sorted_instance
+	}
+	fn nth(&self, index: usize) -> Instance {
+		Instance {
+			feature: [self.x[index], self.y[index], self.z[index], self.w[index]],
+			result: self.result[index],
+		}
+	}
+
+	fn len(&self) -> usize {
+		self.result.len()
+	}
+
+	fn split_at(&self, split_index: usize) -> (Self, Self) {
+		let x = self.x.split_at(split_index);
+		let y = self.y.split_at(split_index);
+		let z = self.z.split_at(split_index);
+		let w = self.w.split_at(split_index);
+		let result = self.result.split_at(split_index);
+		(
+			Self {
+				x: x.0.to_vec(),
+				y: y.0.to_vec(),
+				z: z.0.to_vec(),
+				w: w.0.to_vec(),
+				result: result.0.to_vec(),
+			},
+			Self {
+				x: x.1.to_vec(),
+				y: y.1.to_vec(),
+				z: z.1.to_vec(),
+				w: w.1.to_vec(),
+				result: result.1.to_vec(),
+			},
+		)
 	}
 }
+
+type Features = [i32; 4];
 
 #[derive(Clone, Debug)]
 pub enum DecisionTreeNode {
@@ -19,14 +107,9 @@ pub enum DecisionTreeNode {
 	Value(i32),
 }
 
-#[derive(Clone, Debug)]
-pub struct DecisionTree {
-	///The index of the feature that splitting is determined for
-	root: DecisionTreeNode,
-}
-impl DecisionTree {
-	fn traverse(&self, features: [i32; 4]) -> i32 {
-		let mut start_node = self.root.clone();
+impl DecisionTreeNode {
+	fn traverse(&self, features: Features) -> i32 {
+		let mut start_node = self.clone();
 		loop {
 			use DecisionTreeNode::*;
 			match start_node {
@@ -45,6 +128,55 @@ impl DecisionTree {
 			}
 		}
 	}
+
+	fn construct(instances: &mut Instances) -> Self {
+		let len = instances.len();
+		if len == 1 {
+			return Self::Value(instances.result[0]);
+		}
+		let mut feature_variance = instances
+			.variance()
+			.into_iter()
+			.enumerate()
+			.collect::<Vec<_>>();
+		feature_variance.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+		let most_varied_feature_index = feature_variance[0].0;
+		instances.sort_by_feature(most_varied_feature_index);
+		let (mut left, mut right) = instances.split_at(len / 2);
+		let root = Self::SplitPoint {
+			feature_index: most_varied_feature_index,
+			critical_value: instances.nth(len / 2).feature[most_varied_feature_index],
+			branch: (
+				Box::new(Self::construct(&mut left)),
+				Box::new(Self::construct(&mut right)),
+			),
+		};
+		root
+	}
+}
+
+#[test]
+fn decision_tree_construction() {
+	let mut instances = Instances {
+		x: vec![1, 2, 3, 4, 5],
+		y: vec![5, 6, 7, 8, 50],
+		z: vec![9, 0, 1, 2, 10],
+		w: vec![3, 4, 5, 6, 3],
+		result: vec![7, 8, 9, 0, -10],
+	};
+	let root = DecisionTreeNode::construct(&mut instances);
+	(0..instances.len()).for_each(|i| {
+		assert_eq!(
+			root.traverse([
+				instances.x[i],
+				instances.y[i],
+				instances.z[i],
+				instances.w[i]
+			]),
+			instances.result[i]
+		)
+	});
+	// panic!("{root:#?}");
 }
 
 #[test]
@@ -78,7 +210,6 @@ fn decision_tree_traversal() {
 			}),
 		),
 	};
-	let tree = DecisionTree { root };
 
 	//       Decision Tree
 	//
@@ -90,10 +221,10 @@ fn decision_tree_traversal() {
 	//                      /     \
 	//                     3       4
 
-	assert_eq!(tree.traverse([4, 3, 0, 0]), 0);
-	assert_eq!(tree.traverse([4, 10, 0, 0]), 1);
-	assert_eq!(tree.traverse([5, 0, 10, 0]), 2);
-	assert_eq!(tree.traverse([5, 0, 11, 1]), 3);
-	assert_eq!(tree.traverse([5, 0, 11, 2]), 4);
-	assert_eq!(tree.traverse([6, 0, 12, 3]), 4);
+	assert_eq!(root.traverse([4, 3, 0, 0]), 0);
+	assert_eq!(root.traverse([4, 10, 0, 0]), 1);
+	assert_eq!(root.traverse([5, 0, 10, 0]), 2);
+	assert_eq!(root.traverse([5, 0, 11, 1]), 3);
+	assert_eq!(root.traverse([5, 0, 11, 2]), 4);
+	assert_eq!(root.traverse([6, 0, 12, 3]), 4);
 }
